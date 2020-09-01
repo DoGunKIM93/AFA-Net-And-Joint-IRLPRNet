7de8cb6ea15bc5dae22e0c200cb55efd647df3b7
DogunKIM <dgkimk@hanwha.com>
2020-09-01 10:17:16 +0000

    added RetinaFace

--- model.py
+++ model.py
@@ -23,6 +23,7 @@ from torch.autograd import Variable,grad
 from torchvision import datasets
 from torchvision import transforms
 from torchvision.utils import save_image
+from torchvision.models import _utils
 from torch.autograd.function import once_differentiable
 from torch.nn.modules.utils import _pair
 from torch.autograd import Function
@@ -43,7 +44,9 @@ from backbone.EfficientNet.EfficientNet import EfficientNetBuilder, EFF_create_c
     #ResNeSt
 from backbone.ResNeSt.resnest import resnest50, resnest101, resnest200, resnest269
 from backbone.ResNeSt.resnet import ResNet, Bottleneck
-
+    #RetinaFace
+from backbone.RetinaFace.RetinaFace import MobileNetV1, FPN, SSH, ClassHead, BboxHead, LandmarkHead
+from backbone.RetinaFace.RetinaFace_config import cfg_mnet, cfg_re50
 
 
 
@@ -1695,4 +1698,89 @@ class Get_gradient_nopadding(nn.Module):
         x = torch.cat([x0, x1, x2], dim=1)
         return x
 
+
+class RetinaFace(nn.Module):
+    def __init__(self, backboneModel = 'mobilenet0.25', phase = 'train'):
+        """
+        :param cfg:  Network related settings.
+        :param phase: train or test.
+        """
+        super(RetinaFace,self).__init__()
+        if backboneModel == "mobilenet0.25":
+            cfg = cfg_mnet
+        elif backboneModel == "resnet50":
+            cfg = cfg_re50
         
+        self.phase = phase
+        backbone = None
+        if cfg['name'] == 'mobilenet0.25':
+            backbone = MobileNetV1()
+            if cfg['pretrain']:
+                checkpoint = torch.load("/home/projSR/dataset/pretrained/"+"mobilenetV1X0.25_pretrain.tar", map_location=torch.device('cpu'))
+                from collections import OrderedDict
+                new_state_dict = OrderedDict()
+                for k, v in checkpoint['state_dict'].items():
+                    name = k[7:]  # remove module.
+                    new_state_dict[name] = v
+                # load params
+                backbone.load_state_dict(new_state_dict)
+        elif cfg['name'] == 'Resnet50':
+            import torchvision.models as models
+            backbone = models.resnet50(pretrained=cfg['pretrain'])
+
+        self.body = _utils.IntermediateLayerGetter(backbone, cfg['return_layers'])
+        in_channels_stage2 = cfg['in_channel']
+        in_channels_list = [
+            in_channels_stage2 * 2,
+            in_channels_stage2 * 4,
+            in_channels_stage2 * 8,
+        ]
+        out_channels = cfg['out_channel']
+        self.fpn = FPN(in_channels_list,out_channels)
+        self.ssh1 = SSH(out_channels, out_channels)
+        self.ssh2 = SSH(out_channels, out_channels)
+        self.ssh3 = SSH(out_channels, out_channels)
+
+        self.ClassHead = self._make_class_head(fpn_num=3, inchannels=cfg['out_channel'])
+        self.BboxHead = self._make_bbox_head(fpn_num=3, inchannels=cfg['out_channel'])
+        self.LandmarkHead = self._make_landmark_head(fpn_num=3, inchannels=cfg['out_channel'])
+
+    def _make_class_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+        classhead = nn.ModuleList()
+        for i in range(fpn_num):
+            classhead.append(ClassHead(inchannels,anchor_num))
+        return classhead
+    
+    def _make_bbox_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+        bboxhead = nn.ModuleList()
+        for i in range(fpn_num):
+            bboxhead.append(BboxHead(inchannels,anchor_num))
+        return bboxhead
+
+    def _make_landmark_head(self,fpn_num=3,inchannels=64,anchor_num=2):
+        landmarkhead = nn.ModuleList()
+        for i in range(fpn_num):
+            landmarkhead.append(LandmarkHead(inchannels,anchor_num))
+        return landmarkhead
+
+    def forward(self,inputs):
+        out = self.body(inputs)
+
+        # FPN
+        fpn = self.fpn(out)
+
+        # SSH
+        feature1 = self.ssh1(fpn[0])
+        feature2 = self.ssh2(fpn[1])
+        feature3 = self.ssh3(fpn[2])
+        features = [feature1, feature2, feature3]
+
+        bbox_regressions = torch.cat([self.BboxHead[i](feature) for i, feature in enumerate(features)], dim=1)
+        classifications = torch.cat([self.ClassHead[i](feature) for i, feature in enumerate(features)],dim=1)
+        ldm_regressions = torch.cat([self.LandmarkHead[i](feature) for i, feature in enumerate(features)], dim=1)
+
+        if self.phase == 'train':
+            output = (bbox_regressions, classifications, ldm_regressions)
+        else:
+            output = (bbox_regressions, F.softmax(classifications, dim=-1), ldm_regressions)
+        return output
