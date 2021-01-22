eaae07fba5c3633a574347d9798623a59cfd2a52
kim.jin <kim.jin@hanwha.com>
2021-01-22 12:44:34 +0000

    add predefined model DMPHN, DeFiAN

--- backbone/predefined/__init__.py
+++ backbone/predefined/__init__.py
@@ -5,7 +5,10 @@ from backbone.predefined.VDSR import VDSR
 from backbone.predefined.EDVR import EDVR
 from backbone.predefined.VESPCN import VESPCN
 from backbone.predefined.SPSR import SPSR_Generator, SPSR_FeatureExtractor, SPSR_Discriminator, SPSR_Get_gradient, SPSR_Get_gradient_nopadding
+from backbone.predefined.DeFiAN import Generator as DeFiAN
 
+##### DEBLUR #####
+from backbone.predefined.DMPHN import DMPHN_Decoder, DMPHN_Encoder
 
 ##### FACE RECOG #####
 from backbone.predefined.RetinaFace import RetinaFace
@@ -23,5 +26,9 @@ __all__ = [ 'ESPCN',
             'SPSR_Generator', 'SPSR_FeatureExtractor', 'SPSR_Discriminator', 'SPSR_Get_gradient', 'SPSR_Get_gradient_nopadding',
             'RetinaFace',
             'ResNeSt',
+            'DeFiAN',
+            'DMPHN_Decoder', 
+            'DMPHN_Encoder'
+
 ]
 
