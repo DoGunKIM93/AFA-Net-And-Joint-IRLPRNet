6d1a0f9147f4681b0a5f87f223ebd1773cf5a85b
DogunKim <dgkimk@hanwha.com>
2021-04-02 02:21:49 +0000

    inference에서 augmenatation 활용하도록 수정

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 """
 inference.py
 """
-version = "1.02.211008"
+version = "1.10.210402"
 
 
 # READ YAML
@@ -10,11 +10,14 @@ version = "1.02.211008"
 
 import os
 import argparse
+import inspect
+import numpy as np
 import torch
 import torchvision.transforms
 import backbone.augmentation as augmentation
 import backbone.predefined
 import backbone.utils as utils
+import time
 
 from collections.abc import Iterable
 from PIL import Image
@@ -22,17 +25,26 @@ from torchvision.utils import save_image
 from backbone.config import Config
 from torch.nn import DataParallel
 
-# GPU 지정
-os.environ["CUDA_VISIBLE_DEVICES"] = str(Config.param.general.GPUNum)
+# Read Augmentations from backbone.augmentation automatically
+AUGMENTATION_DICT = dict(
+    x for x in inspect.getmembers(augmentation) if (not x[0].startswith("_")) and (inspect.isfunction(x[1]))
+)
+
+# GPU Setting
+os.environ["CUDA_VISIBLE_DEVICES"] = str(Config.inference.general.GPUNum)
 
 
 def _loadModel(inferencePresetName):
+    # model informations
+    model_name = Config.inferenceDict["model"][inferencePresetName]["name"]
+    model_param = Config.inferenceDict["model"][inferencePresetName]["param"]
+    model_weight = Config.inferenceDict["model"][inferencePresetName]["weight"]
 
     # import module by string
-    module = getattr(backbone.predefined, Config.inferenceDict[inferencePresetName]["model"])
+    module = getattr(backbone.predefined, model_name)
 
     # load model network with params
-    paramList = str(Config.inferenceDict[inferencePresetName]["param"]).replace(" ", "").split(",")
+    paramList = str(model_param).replace(" ", "").split(",")
     model = module(
         *list(
             (x if x.replace(".", "", 1).isdigit() is False else (int(x) if x.find(".") is -1 else float(x)))
@@ -43,7 +55,7 @@ def _loadModel(inferencePresetName):
     model = DataParallel(model)
 
     # load checkpoint weight
-    checkpoint = torch.load(Config.param.data.path.pretrainedPath + Config.inferenceDict[inferencePresetName]["weight"])
+    checkpoint = torch.load(Config.inference.data.path.pretrainedPath + model_weight)
     # model.load_state_dict(checkpoint['model'],strict=True)
     while(True):
         try:
@@ -98,11 +110,18 @@ def _loadModel(inferencePresetName):
             break
 
     model.eval()
+
     print(
-        f"{Config.inferenceDict[inferencePresetName]['model']} Loaded with {mthd} mode."
+        f"{model_name} Loaded with {mthd} mode."
         if mthd != "FAILED"
-        else f"{Config.inferenceDict[inferencePresetName]['model']} Load Failed."
+        else f"{model_name} Load Failed."
     )
+
+    paramSize = 0
+    for parameter in model.parameters():
+        paramSize = paramSize + np.prod(np.array(parameter.size()))
+    print(f"parameter size of {model_name} : {str(paramSize)}")
+
     return model
 
 
@@ -123,8 +142,34 @@ def _convertTensorTo(inp, outputType):
     return out
 
 
-def inferenceSingle(inp, inferencePresetName, model=None, outputType=None, outputPath=None):
+def _applyAugmentationFunction(tnsr, augmentationFuncStr: str):
 
+    return _applyAugmentationFunctionFunc(tnsr, augmentationFuncStr)
+
+
+def _applyAugmentationFunctionFunc(tnsr, augmentationFuncStr: str):
+
+    assert (
+        augmentationFuncStr.split("(")[0] in AUGMENTATION_DICT.keys()
+    ), "inference.py :: invalid Augmentation Function!! chcek inference.yaml."
+
+    augFunc = AUGMENTATION_DICT[augmentationFuncStr.split("(")[0]]
+    args = []
+    for x in list(filter(lambda y: y != "", augmentationFuncStr.split("(")[1][:-1].replace(" ", "").split(","))):
+        if (x[1:] if x[0] == "-" else x).replace(".", "", 1).isdigit() is False:
+            args.append(str(x))
+        else:
+            if x.find(".") == -1:
+                args.append(int(x))
+            else: 
+                args.append(float(x))
+    
+    tnsr = augFunc(tnsr, *args)
+
+    return tnsr
+
+
+def inferenceSingle(inp, inferencePresetName, model=None, outputType=None, outputPath=None):
     print("Processing Input...")
     # process input
     if isinstance(inp, str):
@@ -143,8 +188,18 @@ def inferenceSingle(inp, inferencePresetName, model=None, outputType=None, outpu
     assert outputType != "FILE" or (
         outputType == "FILE" and outputPath is not None
     ), "inference.py :: outputPath must be exist if outputType is 'FILE'"
-    inp = augmentation.toTensor([inp])[0].unsqueeze(0).cuda()
-    inp = inp * 2 - 1 if Config.inferenceDict[inferencePresetName]["valueRangeType"] == "-1~1" else torch.round(inp * 255) if Config.inferenceDict[inferencePresetName]["valueRangeType"] == "0~255" else inp
+
+    ##backend-server에서 어느 부분까지 커버할지, inference는 어느 부분까지 공용으로 사용할지 고려해서 data input 부분 정리하기 (bsae64, PIL, tensor 등)
+    model_augmentation = Config.inferenceDict["model"][inferencePresetName]["augmentation"]
+    model_valueRangeType = Config.inferenceDict["model"][inferencePresetName]["valueRangeType"]
+    
+    # augmentation for input image
+    for augmentationFuc in model_augmentation:
+        inp = _applyAugmentationFunction([inp], augmentationFuc)
+        inp = inp[0]
+    
+    inp = inp.unsqueeze(0).cuda()
+    inp = inp * 2 - 1 if model_valueRangeType == "-1~1" else torch.round(inp * 255) if model_valueRangeType == "0~255" else inp
 
     if model is None:
         # load Model
@@ -153,6 +208,7 @@ def inferenceSingle(inp, inferencePresetName, model=None, outputType=None, outpu
 
     # inference
     print("Inferencing...")
+    timePerInference = time.perf_counter()  # 1 inference 당 시간    
     out = _modelInference(inp, model)
 
     # convert to output
@@ -163,23 +219,28 @@ def inferenceSingle(inp, inferencePresetName, model=None, outputType=None, outpu
                 {"Result": x}, 
                 f'{outputPath.split(".")[0]}-{i}.{outputPath.split(".")[1]}', 
                 caption=False, 
-                valueRangeType=Config.inferenceDict[inferencePresetName]["valueRangeType"]
+                valueRangeType=model_valueRangeType
             )
             for i, x in enumerate(out)
         ] if isinstance(out, tuple) or isinstance(out, list) else utils.saveImageTensorToFile(
             {"Result": out}, 
             outputPath, 
             caption=False, 
-            valueRangeType=Config.inferenceDict[inferencePresetName]["valueRangeType"]
+            valueRangeType=model_valueRangeType
         )
         # original save
         utils.saveImageTensorToFile(
             {"Result": inp},
             f'{outputPath.split(".")[0]}-original.{outputPath.split(".")[1]}',
             caption=False,
-            valueRangeType=Config.inferenceDict[inferencePresetName]["valueRangeType"]
+            valueRangeType=model_valueRangeType
         )
         print("Saved.")
+
+        oldtimePerInference = timePerInference
+        timePerInference = time.perf_counter()
+        print(f"Time per inference: {(timePerInference - oldtimePerInference):.2f} sec                    ")
+
         return
     else:
         rst = _convertTensorTo(out, outputType)
